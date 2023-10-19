ef922490463b106ae3ef47f871eb628966d1854b
jferdinand678 <147880350+jferdinand678@users.noreply.github.com>
2023-10-19 15:53:54 -0600

    Update software.ps1

--- tasks/software.ps1
+++ tasks/software.ps1
@@ -1,1 +1,4 @@
+$ID = "$((Get-Date).ToString('yy'))$((Get-Date).ToString('MM'))$((Get-Date).ToString('dd'))$((Get-Date).ToString('HH'))$((Get-Date).ToString('mm'))"
+Start-Transcript -Path "D:\log\software_$ID.log"
 winget upgrade --all
+Stop-Transcript
