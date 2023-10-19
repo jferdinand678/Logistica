defc5193655685c0daac3e04ca4b193e56883cf7
jferdinand678 <147880350+jferdinand678@users.noreply.github.com>
2023-10-19 15:44:41 -0600

    Update update.ps1

--- tasks/update.ps1
+++ tasks/update.ps1
@@ -1,3 +1,5 @@
-Start-Transcript -Path "D:\log\update.log"
+$ID = "$((Get-Date).ToString('yy'))$((Get-Date).ToString('MM'))$((Get-Date).ToString('dd'))$((Get-Date).ToString('HH'))$((Get-Date).ToString('mm'))"
+
+Start-Transcript -Path "D:\log\update_$ID.log"
 git pull
 Stop-Transcript
