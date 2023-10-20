31a356a3f1cdb1917cf9decf52b73d5be51c7803
jferdinand678 <147880350+jferdinand678@users.noreply.github.com>
2023-10-20 10:46:19 -0600

    network

--- tasks/network.ps1
+++ tasks/network.ps1
@@ -1,5 +1,2 @@
 $ID = "$((Get-Date).ToString('yy'))$((Get-Date).ToString('MM'))$((Get-Date).ToString('dd'))$((Get-Date).ToString('HH'))$((Get-Date).ToString('mm'))"
-Start-Transcript -Path "D:\log\software_$ID.log"
-winget upgrade --all
-Stop-Transcript
-
+nmap -sn 192.168.1.0/24 -oN D:\log\hosts_$ID.txt  
