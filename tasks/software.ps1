e0e9856fb49a61aec79091ed38dffc404b2193d9
jferdinand678 <147880350+jferdinand678@users.noreply.github.com>
2023-10-24 13:45:42 -0600

    uninstall

--- tasks/software.ps1
+++ tasks/software.ps1
@@ -11,7 +11,78 @@ $ID = "$((Get-Date).ToString('yy'))$((Get-Date).ToString('MM'))$((Get-Date).ToSt
 Start-Transcript -Path "D:\log\software_$ID.log"
 
 winget install Microsoft.Powershell Google.Chrome Google.GoogleDrive Google.EarthPro Google.ChromeRemoteDesktop --disable-interactivity
-winget uninstall Microsoft.549981C3F5F10--disable-interactivity
+
+winget uninstall cortana
+winget uninstall skype
+
+##Xbox Applications
+winget uninstall Microsoft.GamingApp_8wekyb3d8bbwe --accept-source-agreements --silent
+winget uninstall Microsoft.XboxApp_8wekyb3d8bbwe --accept-source-agreements --silent
+winget uninstall Microsoft.Xbox.TCUI_8wekyb3d8bbwe --accept-source-agreements --silent
+winget uninstall Microsoft.XboxSpeechToTextOverlay_8wekyb3d8bbwe --accept-source-agreements --silent
+winget uninstall Microsoft.XboxIdentityProvider_8wekyb3d8bbwe --accept-source-agreements --silent
+winget uninstall Microsoft.XboxGamingOverlay_8wekyb3d8bbwe --accept-source-agreements --silent
+winget uninstall Microsoft.XboxGameOverlay_8wekyb3d8bbwe --accept-source-agreements --silent
+
+##Groove Music
+winget uninstall Microsoft.ZuneMusic_8wekyb3d8bbwe --accept-source-agreements --silent
+
+##Feedback Hub
+winget uninstall Microsoft.WindowsFeedbackHub_8wekyb3d8bbwe --accept-source-agreements --silent
+
+##Microsoft-Tips...
+winget uninstall Microsoft.Getstarted_8wekyb3d8bbwe --accept-source-agreements --silent
+
+##3D Viewer
+winget uninstall 9NBLGGH42THS --accept-source-agreements --silent
+
+##Solitaire
+winget uninstall Microsoft.MicrosoftSolitaireCollection_8wekyb3d8bbwe --accept-source-agreements --silent
+
+##Paint-3D...
+winget uninstall 9NBLGGH5FV99 --accept-source-agreements --silent
+
+##Weather 
+winget uninstall Microsoft.BingWeather_8wekyb3d8bbwe --accept-source-agreements --silent
+
+##Mail / Calendar
+winget uninstall microsoft.windowscommunicationsapps_8wekyb3d8bbwe --accept-source-agreements --silent
+
+##Your Phone
+winget uninstall Microsoft.YourPhone_8wekyb3d8bbwe --accept-source-agreements --silent
+
+##People
+winget uninstall Microsoft.People_8wekyb3d8bbwe --accept-source-agreements --silent
+
+##MS Pay 
+winget uninstall Microsoft.Wallet_8wekyb3d8bbwe --accept-source-agreements --silent
+
+##MS Maps
+winget uninstall Microsoft.WindowsMaps_8wekyb3d8bbwe --accept-source-agreements --silent
+
+##OneNote
+winget uninstall Microsoft.Office.OneNote_8wekyb3d8bbwe --accept-source-agreements --silent
+
+##MS Office
+winget uninstall Microsoft.MicrosoftOfficeHub_8wekyb3d8bbwe --accept-source-agreements --silent
+
+##Voice Recorder
+winget uninstall Microsoft.WindowsSoundRecorder_8wekyb3d8bbwe --accept-source-agreements --silent
+
+##Movies & TV
+winget uninstall Microsoft.ZuneVideo_8wekyb3d8bbwe --accept-source-agreements --silent
+
+##Mixed Reality-Portal
+winget uninstall Microsoft.MixedReality.Portal_8wekyb3d8bbwe --accept-source-agreements --silent
+
+##Sticky Notes...
+winget uninstall Microsoft.MicrosoftStickyNotes_8wekyb3d8bbwe --accept-source-agreements --silent
+
+##Get Help
+winget uninstall Microsoft.GetHelp_8wekyb3d8bbwe --accept-source-agreements --silent
+
+##OneDrive
+winget uninstall Microsoft.OneDrive --accept-source-agreements --silent
 
 Stop-Transcript
 
