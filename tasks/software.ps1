f7381c43aa843fec1b102500494a329382892dc3
jferdinand678 <147880350+jferdinand678@users.noreply.github.com>
2023-10-24 13:17:59 -0600

    add software

--- tasks/software.ps1
+++ tasks/software.ps1
@@ -10,5 +10,7 @@
 $ID = "$((Get-Date).ToString('yy'))$((Get-Date).ToString('MM'))$((Get-Date).ToString('dd'))$((Get-Date).ToString('HH'))$((Get-Date).ToString('mm'))"
 Start-Transcript -Path "D:\log\software_$ID.log"
 winget upgrade --all
+winget install Microsoft.Powershell Google.Chrome Google.GoogleDrive Google.GoogleDesktop ankurk91.GoogleChatElectron arjun-g.google-meet-desktop Google.EarthPro alexkim205.g-desktop-suite klinker24.google-calendar-desktop timche.gmail-deskt  Google.ChromeRemoteDesktop 
+
 Stop-Transcript
 
