0ed1c2d51955d56118303c75410783d5b20b2b39
jferdinand678 <147880350+jferdinand678@users.noreply.github.com>
2023-10-24 13:39:48 -0600

    Update software.ps1

--- tasks/software.ps1
+++ tasks/software.ps1
@@ -10,7 +10,8 @@
 $ID = "$((Get-Date).ToString('yy'))$((Get-Date).ToString('MM'))$((Get-Date).ToString('dd'))$((Get-Date).ToString('HH'))$((Get-Date).ToString('mm'))"
 Start-Transcript -Path "D:\log\software_$ID.log"
 
-winget install Microsoft.Powershell Google.Chrome Google.GoogleDrive Google.GoogleDesktop Google.EarthPro Google.ChromeRemoteDesktop 
+winget install Microsoft.Powershell Google.Chrome Google.GoogleDrive Google.EarthPro Google.ChromeRemoteDesktop --disable-interactivity
+winget uninstall Microsoft.549981C3F5F10--disable-interactivity
 
 Stop-Transcript
 
