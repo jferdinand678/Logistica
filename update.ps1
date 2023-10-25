0f87bd143d12b454998953393be07772ab093890
jferdinand678 <147880350+jferdinand678@users.noreply.github.com>
2023-10-25 09:30:34 -0600

    Update update.ps1

--- update.ps1
+++ update.ps1
@@ -1,5 +1,6 @@
 $ID = "$((Get-Date).ToString('yy'))$((Get-Date).ToString('MM'))$((Get-Date).ToString('dd'))$((Get-Date).ToString('HH'))$((Get-Date).ToString('mm'))"
 cd "C:\Users\Public\scripts\Logistica"
-Start-Transcript -OutputDirectory c:\temp\Logfiles -Append
+
+Start-Transcript -Path "D:/log/update$ID.log" -Append
 git pull
 Stop-Transcript
