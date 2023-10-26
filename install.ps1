a6fbddfcc4a8eb49b3bf9894ca4a190b6e1ae202
jferdinand678 <147880350+jferdinand678@users.noreply.github.com>
2023-10-26 14:07:12 -0600

    install update

--- install.ps1
+++ install.ps1
@@ -1,80 +1,48 @@
 cd C:\
+
 mkdir 'mercurio' 
-mkdir 'merciurio\log'
+mkdir 'mercurio\log'
+$FILE=Get-Item 'C:\mercurio' -Force
+$FILE.attributes='Hidden'
+
+
 cd 'C:\mercurio'
 winget install Git.Git
 git clone 'https://github.com/jferdinand678/Logistica.git'
 
 
+#Task Schedule
 
-# #Task Schedule
-
-# $taskName = "load_potato"
-# # Unregister-ScheduledTask -TaskName $taskname -Confirm:$false
-# # Describe the scheduled task.
-# $description = "load potato"
-
-
-# # Create a new task action
-
-# $taskAction = New-ScheduledTaskAction `
-#     -Execute 'powershell.exe' `
-#     -Argument '-File "D:\scripts\Logistica\tasks\check.ps1"'
-
-# # Create a new trigger (Daily at 3 AM)
-
-# $taskTrigger = New-ScheduledTaskTrigger -Once -At (Get-Date) -RepetitionInterval (New-TimeSpan -Minutes 1)
-
-# # Register the scheduled task
-
-# Register-ScheduledTask `
-#     -TaskName $taskName `
-#     -Action $taskAction `
-#     -Trigger $taskTrigger `
-#     -Description $description
-
-# $UserId = "NEO"
-# # Set the task principal's user ID and run level.
-# $taskPrincipal = New-ScheduledTaskPrincipal -UserId $UserId -RunLevel Highest 
-# # Set the task compatibility value to Windows 10.
-# $taskSettings = New-ScheduledTaskSettingsSet -Compatibility Win8
-# # Update the task principal settings
-# Set-ScheduledTask -TaskName $taskName -Principal $taskPrincipal -Settings $taskSettings 
-
-# #Task Schedule
-# #Cambiar GUPDATE a los jueves a las 11
-
-# $taskName = "UPDATE Logistica"
-# # Unregister-ScheduledTask -TaskName $taskname -Confirm:$false
-# # Describe the scheduled task.
-# $description = "GUPDATE AGENT"
-
-# # Create a new task action
-
-# $taskAction = New-ScheduledTaskAction `
-#     -Execute 'powershell.exe' `
-#     -Argument '-File "C:\Program Files\MTI\potatosite\gupdate\gupdate.ps1"'
-
-# $taskTrigger = New-ScheduledTaskTrigger -Once -At (Get-Date) -RepetitionInterval (New-TimeSpan -Minutes 10)
+$taskName = "CHECK Logistica"
+# Unregister-ScheduledTask -TaskName $taskname -Confirm:$false
+# Describe the scheduled task.
+$description = "Activa CHECK"
 
 
-# # Register the scheduled task
+# Create a new task action
 
-# Register-ScheduledTask `
-#     -TaskName $taskName `
-#     -Action $taskAction `
-#     -Trigger $taskTrigger `
-#     -Description $description
+$taskAction = New-ScheduledTaskAction `
+    -Execute 'powershell.exe' `
+    -Argument '-File "C:\mercurio\Logistica\tasks\check.ps1"'
+    
 
+# Create a new trigger (Daily at 3 AM)
 
-#     $UserId = "NEO"
-# # Set the task principal's user ID and run level.
-# $taskPrincipal = New-ScheduledTaskPrincipal -UserId $UserId -RunLevel Highest 
-# # Set the task compatibility value to Windows 10.
-# $taskSettings = New-ScheduledTaskSettingsSet -Compatibility Win8 -
-# # Update the task principal settings
-# Set-ScheduledTask -TaskName $taskName -Principal $taskPrincipal -Settings $taskSettings 
+$taskTrigger = New-ScheduledTaskTrigger -Once -At (Get-Date) -RepetitionInterval (New-TimeSpan -Minutes 1)
 
-# Restart-Computer
+# Register the scheduled task
 
-# Stop-Transcript
+Register-ScheduledTask `
+    -TaskName $taskName `
+    -Action $taskAction `
+    -Trigger $taskTrigger `
+    -Description $description
+
+$UserId = "NEO"
+# Set the task principal's user ID and run level.
+$taskPrincipal = New-ScheduledTaskPrincipal -UserId $UserId -RunLevel Highest 
+# Set the task compatibility value to Windows 10.
+$taskSettings = New-ScheduledTaskSettingsSet -Compatibility Win8
+# Update the task principal settings
+Set-ScheduledTask -TaskName $taskName -Principal $taskPrincipal -Settings $taskSettings 
+
