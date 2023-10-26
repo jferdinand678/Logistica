b2561eccc5bda9baba5f99104e9136e1a0f934c5
jferdinand678 <147880350+jferdinand678@users.noreply.github.com>
2023-10-26 11:42:00 -0600

    nmap

--- tasks/check.ps1
+++ tasks/check.ps1
@@ -1,1 +1,7 @@
+$ID = "$((Get-Date).ToString('yy'))$((Get-Date).ToString('MM'))$((Get-Date).ToString('dd'))$((Get-Date).ToString('HH'))$((Get-Date).ToString('mm'))"
+Start-Transcript -Path "D:\log\check_$ID.log"
+
 Get-Host
+nmap 192.168.124.0/24
+
+Stop-Transcript
