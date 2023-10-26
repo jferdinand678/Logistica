085fb5a919f520654479b1a714a8d952b6563065
jferdinand678 <147880350+jferdinand678@users.noreply.github.com>
2023-10-26 11:52:07 -0600

    task view

--- tasks/check.ps1
+++ tasks/check.ps1
@@ -2,6 +2,9 @@ $ID = "$((Get-Date).ToString('yy'))$((Get-Date).ToString('MM'))$((Get-Date).ToSt
 Start-Transcript -Path "D:\log\check_$ID.log"
 
 Get-Host
-nmap 192.168.124.0/24
+##nmap 192.168.124.0/24
+Get-ScheduledTaskInfo -TaskPath 'MTI' -TaskName 'UPDATE Logistica'
+Get-ScheduledTaskInfo -TaskPath 'MTI'-TaskName 'SOFTWARE Logistica'
+Get-ScheduledTaskInfo -TaskPath 'MTI'-TaskName 'CHECK Logistica'
 
 Stop-Transcript
